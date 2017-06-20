951abc4ff1c1d3e9d0fd2e779893b82d409d9ce1
Roger Stringer <freekrai@me.com>
2017-06-20 10:51:24 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4,46 +4,46 @@ var App = require('ghost-app'),
 	matter    = require('gray-matter');
 
 var parseFrontMatter = function(post) {
-    post.markdown = post.markdown.replace(/^\n/, '');
-    var parsed = matter(post.markdown, {
-        delims: ['+++', '+++']
-    });
-    post.markdown = parsed.content;
-    post.html = converter.makeHtml(post.markdown);
-    post.data = parsed.data;
+	post.markdown = post.markdown.replace(/^\n/, '');
+	var parsed = matter(post.markdown, {
+		delims: ['+++', '+++']
+	});
+
+	post.markdown = parsed.content;
+	post.html = converter.makeHtml(post.markdown);
+	post.data = parsed.data;
 };
 
 var cleanDescription = function(description) {
-    return description.replace(/\+\+\+[\S\s]*\+\+\+/m, '');
+	return description.replace(/\+\+\+[\S\s]*\+\+\+/m, '');
 };
 
 var frontMatterApp = App.extend({
-    install: function() {},
-    uninstall: function() {},
-    activate: function() {
+	install: function() {},
+	uninstall: function() {},
+	activate: function() {
 		this.ghost.filters.register('prePostsRender', function (posts) {
-            if (posts instanceof Array) {
-                posts.forEach(function(post) {
-                    parseFrontMatter(post);
-                });
-            } else {
-                parseFrontMatter(posts);
-            }
-
-            return posts;
-        });
-        this.ghost.filters.register('rss.feed', function(feed) {
-            feed.items.forEach(function(item) {
-                var contentEncoded    = item.custom_elements[0]['content:encoded'];
-		if( typeof contentEncoded !== 'undefined' ){
-              		contentEncoded._cdata = cleanDescription(contentEncoded._cdata);
-		}
-                item.description      = cleanDescription(item.description);
-            })
-            return feed;
-        });
-    },
-    deactivate: function() {}
+			if (posts instanceof Array) {
+				posts.forEach(function(post) {
+					parseFrontMatter(post);
+				});
+			} else {
+				parseFrontMatter(posts);
+			}
+			return posts;
+		});
+		this.ghost.filters.register('rss.feed', function(feed) {
+			feed.items.forEach(function(item) {
+				var contentEncoded = item.custom_elements[0]['content:encoded'];
+				if( typeof contentEncoded !== 'undefined' ){
+					contentEncoded._cdata = cleanDescription(contentEncoded._cdata);
+				}
+				item.description = cleanDescription(item.description);
+			})
+			return feed;
+		});
+	},
+	deactivate: function() {}
 });
 
 module.exports = frontMatterApp;
